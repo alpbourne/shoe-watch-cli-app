b2344a4ee4e924292608dad91502c689bb5a4b1f
alpbourne <alpbourne@gmail.com>
2017-10-20 18:20:21 +0000

    started outline of ShoeWatchCliApp::CLI; added the call, start, and greeting methods skeleton

--- lib/shoe_watch_cli_app/cli.rb
+++ lib/shoe_watch_cli_app/cli.rb
@@ -1,3 +1,14 @@
 class ShoeWatchCliApp::CLI
+  def call
+    start
+  end
 
+  def start
+    greeting
+  end
+
+  def greeting
+    puts "Welcome to Shoe Watch! Please select the type of shoe you would like to see the latest trend in?"
+    input = gets.strip.downcase
+  end
 end
