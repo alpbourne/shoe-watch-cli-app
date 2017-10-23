cc3b79283565c92aaaf4969361d808e6510ceaea
alpbourne <alpbourne@gmail.com>
2017-10-23 14:49:46 +0000

    added self.running_shoes method

--- lib/shoe_watch_cli_app/shoes.rb
+++ lib/shoe_watch_cli_app/shoes.rb
@@ -1,6 +1,14 @@
 class ShoeWatchCliApp::Shoes
   attr_accessor :name, :brand, :price, :description, :url
 
+  def self.running_shoes
+    running_shoes = []
+    running_shoes << self.scrape_addidas_running
+    running_shoes << self.scrape_nike_running
+    running_shoes << self.scrape_under_armour_running
+    running_shoes
+  end
+
   def self.scrape_addidas_running
 
   end
