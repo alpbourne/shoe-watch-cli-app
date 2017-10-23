87a2a44f81f96eca5da5ac736be148001e01adc3
alpbourne <alpbourne@gmail.com>
2017-10-23 16:26:48 +0000

    corrected file relationship errors

--- lib/shoe_watch_cli_app.rb
+++ lib/shoe_watch_cli_app.rb
@@ -2,9 +2,9 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require_relative "shoe_watch_cli_app/version"
-require_relative "shoe_watch_cli_app/cli"
-require_relative "shoe_watch_cli_app/shoes"
+require_relative "./shoe_watch_cli_app/version"
+require_relative "./shoe_watch_cli_app/cli"
+require_relative "./shoe_watch_cli_app/shoes"
 
 module ShoeWatchCliApp
 end
