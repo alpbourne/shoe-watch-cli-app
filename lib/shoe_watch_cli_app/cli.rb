b1c4f059768f0b022acd878c1ff86918861b6e23
alpbourne <alpbourne@gmail.com>
2017-10-25 22:06:26 +0000

    deleted commented out code in cli.rb

--- lib/shoe_watch_cli_app/cli.rb
+++ lib/shoe_watch_cli_app/cli.rb
@@ -70,42 +70,5 @@ class ShoeWatchCliApp::CLI
       end
     end
   end
-
-
-  # def decision
-  #   @running_shoes = ShoeWatchCliApp::Shoes.running
-  #   @lifstyle_shoes = ShoeWatchCliApp::Shoes.running
-  #   input = nil
-  #   while input != "exit"
-  #     puts "Please enter the number of the shoe type you would like to see the latest trend in or 'list'for your options or type exit."
-  #     input = gets.strip.downcase
-  #     input.to_i == 1
-  #       @running_shoes.each.with_index(1) do |shoe, i|
-  #       puts "#{i}. #{shoe.brand} - #{shoe.name}"
-  #     elsif input.to_i == 1
-  #       @lifestyle_shoes.each.with_index(1) do |shoe, i|
-  #       puts "#{i}. #{shoe.brand} - #{shoe.name}"
-  #     elsif "list"
-  #       option_list
-  #     elsif "exit"
-  #       puts "Thank you for visiting Shoe Watch! Good luck in all your shoe endeavors!"
-  #     else
-  #       puts "Input not valid."
-  #     end
-  #   end
-  # end
-
-  # def shoe_list
-  #   @running_shoes = ShoeWatchCliApp::Shoes.running
-  #   if @input == "1"
-  #     @running_shoes.each do |shoe|
-  #       puts "#{shoe.brand}"
-  #       puts "#{shoe.name}"
-  #       puts "#{shoe.description}"
-  #       puts "#{shoe.price}"
-  #       puts "#{shoe.description}"
-  #     end
-  #   end
-  # end
-
+  
 end
